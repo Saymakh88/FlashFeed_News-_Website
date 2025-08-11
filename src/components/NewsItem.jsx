14fe2341ec1eaa4c8ea6b977e4a8c7cdc37c4b0d
Saymakh88 <ksayma983@gmail.com>
2025-08-11 22:13:26 +0530

    added screenshot

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -9,6 +9,7 @@ const NewsItem = ({ title, description, src, url }) => {
           <a
             className="inline-block mt-3 bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg"
             href={url}
+            
           >
             Read More
           </a>
